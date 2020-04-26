c5d5a68d84f409d3cb0a4c106b6e14ec1f41526c
manny i. agbayani <serverless.yoda@gmail.com>
2020-04-26 18:02:52 +1200

    Add project files.

---
+++ RMQ.Domain/CardType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMQ.Domain
+{
+    public class CardType
+    {
+        public int Id { get; set; }
+        public string CardTypeName { get; set; }
+    }
+}
---
+++ RMQ.Domain/PaymentTransaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RMQ.Domain
+{
+    public class PaymentTransaction
+    {
+        public int  Id { get; set; }
+        public string  Name { get; set; }
+        public string CardNumber { get; set; }
+        public int CardTypeId { get; set; }
+        
+    }
+}
